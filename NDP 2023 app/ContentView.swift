1c9d69ceae5260909f9002bb4dbf74070c540f7c
Arthan Tjen <arthan.nathaniel.tjen@sji.edu.sg>
2023-07-29 10:19:40 +0800

    Use this plsssssss

--- NDP 2023 app/ContentView.swift
+++ NDP 2023 app/ContentView.swift
@@ -1,21 +1,55 @@
-//
-//  ContentView.swift
-//  NDP 2023 app
-//
-//  Created by T Krobot on 29/7/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    struct Question {
+        var question: String
+        var answer: String
+    }
+    
+    var questions = [
+        Question(question: "What is the famous shopping street in SG", answer: "Orchard Road"),
+        Question(question: "What is SG's national flower?", answer: "Vanda Miss Joquim"),
+        Question(question: "What does NDP stand for?", answer: "National Day Parade"),
+        Question(question: "Who is Singapore's Prime Minister?", answer: "Lee Hsien Loong"),
+        Question(question: "Who is the President of SG", answer: "Halimah Yacob"), Question(question: "Name the reservoir in Singapore, popular for water sports activities and surrounded by nature trails", answer: "MacRitchie Resevior"), Question(question: "Name the offshore island in Singapore that is an important landfill so we all don't suffer by ChatGPT's takeover of the world.", answer: "Pulau Semakau")
+    ]
+    
+    @State private var showButton = true
+    @State private var textEntered = ""
+    @State private var questionIndex = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, arthan!")
+            if showButton {
+                Button(action: {
+                    showButton = false
+                }) {
+                    Text("Click to start the game")
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .font(.largeTitle)
+                        .frame(width: 250, height: 100)
+                        .padding(10)
+                }
+            } else {
+                Form {
+                    Section(header: Text(questions[questionIndex].question)) {
+                        TextField("Your Answer", text: $textEntered).textFieldStyle(.roundedBorder)
+                    }
+                    if textEntered != ""{
+                        Button{
+                            
+                        }label: {
+                            Text("Submit")
+                        }
+                    }
+                }
+                if  questions[questionIndex].answer == textEntered{
+                    Text("You Are CORRECT!!")
+                }
+            }
         }
-        .padding()
     }
 }
 
