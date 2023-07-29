25099822c7ce7b8d5d2f482ba41c4c2f743299ec
eeyer-nzo <enzooh8888@gmail.com>
2023-07-29 10:03:15 +0800

    Initial Commit

---
+++ NDP 2023 app/NDP_2023_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDP_2023_appApp.swift
+//  NDP 2023 app
+//
+//  Created by T Krobot on 29/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NDP_2023_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
